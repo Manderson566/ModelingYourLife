acf07a994986db51c18c49abcaadcb5630ab2c57
Michael Anderson <Micmail566@gmail.com>
2017-01-17 23:14:42 -0500

    added turn counter, and, retry also fixed random word

--- ModelingYourLife/Program.cs
+++ ModelingYourLife/Program.cs
@@ -10,42 +10,92 @@ namespace ModelingYourLife
     {
         static void Main(string[] args)
         {
-            Objects rndObject = new Objects("Mystery object");
-            RunningMan runner = new RunningMan("Runner");
-            //Console.WriteLine(rndObject);
-            int turns = 5;
-            string randomObject = rndObject.ToString();
-            while (runner.currentState == State.Standing && turns >0 )
-                if ((randomObject == "Ladder") || (randomObject == "Bridge"))
+            bool retry = true;
+            while (retry)
+            {
+                Console.WriteLine("Enter Your Running Man Name");
+                string playerName = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("");
+                Console.WriteLine($"Welcome {playerName} to Running Man! To Play Type Jump or Duck Into The Console.");
+                Console.WriteLine("You Are Trying To Jump Over Or Duck Under Obsticals Before You See Them.");
+                Console.WriteLine("Your Charater has 3 Stamina If You Trip Too Many Times He Falls.");
+                Console.WriteLine("");
+
+                
+
+                RunningMan runner = new RunningMan(playerName);
+                //Console.WriteLine(rndObject);
+                int turns = 5;
+                while (runner.currentState == State.Standing && turns > 0)
                 {
-                    Console.WriteLine("do you want to jump or duck?");
+                    Objects rndObject = new Objects("Mystery object");
+                    string randomObject = rndObject.ToString();
+                    string jump = "jump";
                     string duck = "duck";
-                    string jumpDuckSelect = Console.ReadLine();
-                    if (jumpDuckSelect == duck.ToLower())
+                    Console.WriteLine($"You have {turns} turns remaining");
+                    if ((randomObject == "Ladder") || (randomObject == "Bridge"))
                     {
-                        Console.WriteLine("A sucsessfull jump");
+                        Console.WriteLine("Do You Want To Jump Or Duck?");
+                        string jumpDuckSelect = Console.ReadLine();
+                        Console.Clear();
+
+                        if (jumpDuckSelect == duck.ToLower())
+                        {
+                            Console.WriteLine($"A Sucsessfull Jump {runner} Stamina Remaining");
+
+                        }
+                        else
+                        {
+                            runner.ReduceStam();
+                            Console.WriteLine($"{runner} Stamina: You Tripped, -1 to Stamina");
+
+                        }
                     }
                     else
                     {
-                        runner.ReduceStam();
-                        Console.WriteLine($"{runner} Stamina: You fell, -1 to Stamina");
+                        Console.WriteLine("Do You Want To Jump Or Duck?");
+                        string jumpDuckSelect = Console.ReadLine();
+                        Console.Clear();
+                        if (jumpDuckSelect == jump.ToLower())
+                        {
+                            Console.WriteLine($"A Sucsessfull Jump {runner} Stamina Remaining");
+
+                        }
+                        else
+                        {
+                            runner.ReduceStam();
+                            Console.WriteLine($"{runner} Stamina: You Tripped, -1 To Stamina");
+
+                        }
+                    }
+                    turns--;
+                    Console.WriteLine($"The Object Was a {rndObject}");
+                    Console.WriteLine("");
+                }
+                if (turns == 0)
+                {
+                    Console.WriteLine("WINNER!!!!!!!!!!!!!!!");
+                    Console.WriteLine("Type ( Y )  To Play Agian");
+                    string playAgainCK = Console.ReadLine();
+                    if (playAgainCK.ToUpper() == "Y")
+                    {
+                        continue;
                     }
                 }
                 else
                 {
-                    Console.WriteLine("do you want to jump or duck?");
-                    string jump = "jump";
-                    string jumpDuckSelect = Console.ReadLine();
-                    if (jumpDuckSelect == jump.ToLower())
+                    Console.WriteLine("YOU FELL! Play Again And Improve Your Skills");
+                    Console.WriteLine("Type ( Y )  To Play Agian");
+                    string playAgainCK = Console.ReadLine();
+                    if (playAgainCK.ToUpper() == "Y")
                     {
-                       Console.WriteLine("A sucsessfull jump");
-                    }
-                    else
-                    {
-                        runner.ReduceStam();
-                        Console.WriteLine($"{runner} Stamina: You fell, -1 to Stamina");
+                        continue;
                     }
                 }
+            }
+            Console.WriteLine("Goodbye");
+            retry = false;
         }
     }
 }
